4a3c1426f19142d4e8dae7f594537250ed53f6f0
preeti664 <preeti.shah664@gmail.com>
2020-09-20 21:59:34 +0530

    d3.js

---
+++ 18-9-2020day16(d3.js)/index.js
@@ -0,0 +1,8 @@
+//selection
+d3.select('h1');
+d3.select('h1').style('color', red)
+//.style is  manipulating css
+
+//chaining methods
+.attr('header')//The attr() method is used to add or update the attribute of the selected elements
+.text('update header');
